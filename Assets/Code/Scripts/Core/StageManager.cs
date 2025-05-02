6aec1ea5a873b9481096c3f7f2e2c64d08f0be66
qawerhjf <gimseungjin460@gmail.com>
2025-05-02 14:35:39 +0900

    유닛중복 방지(수정예정)

--- Assets/Code/Scripts/Core/StageManager.cs
+++ Assets/Code/Scripts/Core/StageManager.cs
@@ -22,11 +22,13 @@ public class StageManager : MonoBehaviour
     // 가상의 평면 충돌체
     private Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
 
+    // 이미 유닛이 설치된 위치 저장용
+    private HashSet<int2> occupiedTiles = new HashSet<int2>();
+
     void Awake()
     {
         if (instance != null && instance != this)
         {
-            // 기존 인스턴스 제거
             Destroy(instance);
         }
         instance = this;
@@ -42,11 +44,21 @@ public class StageManager : MonoBehaviour
             {
                 Vector3 hitPoint = ray.GetPoint(enter);
                 int2 gridPos = WorldToGridPosition(hitPoint);
-                Debug.Log(gridPos);
-                GameObject newObject = Instantiate(TestPrefab);
-                newObject.transform.position += GridToWorldPosition(gridPos);
-            }
 
+                // 중복 체크
+                if (occupiedTiles.Contains(gridPos))
+                {
+                    Debug.Log("이미 유닛이 있는 타일입니다.");
+                    return;
+                }
+
+                // 유닛 생성
+                GameObject newObject = Instantiate(TestPrefab);
+                newObject.transform.position = GridToWorldPosition(gridPos) + Vector3.up * 0.5f;
+
+                // 좌표 기록
+                occupiedTiles.Add(gridPos);
+            }
         }
     }
 
@@ -59,7 +71,8 @@ public class StageManager : MonoBehaviour
 
     public Vector3 GridToWorldPosition(int2 gridPos)
     {
-        Vector3 worldPos = new Vector3(gridPos.x * cellSize.x, 0, gridPos.y * cellSize.y) + new Vector3(gridPos.x * cellGap.x, 0, gridPos.y * cellGap.y);
+        Vector3 worldPos = new Vector3(gridPos.x * cellSize.x, 0, gridPos.y * cellSize.y)
+                         + new Vector3(gridPos.x * cellGap.x, 0, gridPos.y * cellGap.y);
         return worldPos;
     }
 }
