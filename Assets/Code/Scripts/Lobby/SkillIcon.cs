c836232993b56b1eaddc2a9cf79a7bb34cfbb12c
9545000 <asd315406@gmail.com>
2025-05-07 12:06:43 +0900

    스킬 아이콘 업데이트

--- Assets/Code/Scripts/Lobby/SkillIcon.cs
+++ Assets/Code/Scripts/Lobby/SkillIcon.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SkillIcon : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public SkillInformation information;
+    public Image iconImage;
+
     private SkillData skillData;
 
     public void SetSkillData(SkillData data)
     {
         skillData = data;
-        //skillData.icon;
+        Sprite sprite = Resources.Load<Sprite>(data.icon.Trim());
+        iconImage.sprite = sprite;
     }
 
     public void OnPointerDown(PointerEventData eventData)
