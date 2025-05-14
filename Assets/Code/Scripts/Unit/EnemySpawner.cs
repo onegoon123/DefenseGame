8ee33f1151a26a668f5eec5e4e25a20a02dd74ff
9545000 <asd315406@gmail.com>
2025-05-14 11:13:56 +0900

    적 스폰 업데이트

--- Assets/Code/Scripts/Unit/EnemySpawner.cs
+++ Assets/Code/Scripts/Unit/EnemySpawner.cs
@@ -5,6 +5,14 @@ using System.Drawing;
 using Unity.Mathematics;
 using UnityEngine;
 
+enum EnemySpawnerState
+{
+    None,
+    Wait,
+    Wave,
+    Clear,
+}
+
 [Serializable]
 public class SpawnData
 {
@@ -26,6 +34,150 @@ public class EnemySpawner : MonoBehaviour
 
     public List<int2> SpawnPoint = new List<int2>();
     public List<EnemyWave> waves = new List<EnemyWave>();
+    public List<GameObject> enemies = new List<GameObject>();
+    
+    private EnemySpawnerState currentState;
+    private EnemyWave currentWave = null;
+    private int waveIndex = 0;
+    private float waitTime = 0;
+
+
+    private void ChangeState(EnemySpawnerState state)
+    {
+        switch (currentState)
+        {
+            case EnemySpawnerState.Wait:
+                WaitEnd();
+                break;
+            case EnemySpawnerState.Wave:
+                WaveEnd();
+                break;
+            case EnemySpawnerState.Clear:
+                ClearEnd();
+                break;
+        }
+        currentState = state;
+        switch (currentState)
+        {
+            case EnemySpawnerState.Wait:
+                WaitStart();
+                break;
+            case EnemySpawnerState.Wave:
+                WaveStart();
+                break;
+            case EnemySpawnerState.Clear:
+                ClearStart();
+                break;
+        }
+    }
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case EnemySpawnerState.Wait:
+                WaitUpdate();
+                break;
+            case EnemySpawnerState.Wave:
+                WaveUpdate();
+                break;
+            case EnemySpawnerState.Clear:
+                ClearUpdate();
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        ChangeState(EnemySpawnerState.Wait);
+    }
+
+
+    // Wait State
+    private void WaitStart()
+    {
+        currentWave = waves[waveIndex];
+        waitTime = currentWave.waveWaitTime;
+    }
+
+    private void WaitUpdate()
+    {
+        waitTime -= Time.deltaTime;
+        if (waitTime < 0)
+        {
+            ChangeState(EnemySpawnerState.Wave);
+        }
+    }
+
+    private void WaitEnd()
+    {
+
+    }
+
+    // Wave State
+    private void WaveStart()
+    {
+
+    }
+
+    private void WaveUpdate()
+    {
+        bool clearWave = true;  // 모든 적이 스폰하여 웨이브가 끝났다면 true가 유지된다
+
+        foreach (SpawnData spawnData in currentWave.spawnDatas)
+        {
+            if (spawnData.count <= 0)
+            {
+                continue;
+            }
+            clearWave = false;
+
+            int2 spawnPos = SpawnPoint[spawnData.point];
+            if (StageManager.instance.units[spawnPos.x, spawnPos.y] == null)
+            {
+                // 유닛 생성
+                SpawnEnemy(spawnPos, spawnData.code);
+                break;
+            }
+        }
+
+        if (clearWave)
+        {
+            if (waves.Count <= ++waveIndex)
+            {
+                ChangeState(EnemySpawnerState.Clear);
+            }
+            else
+            {
+                ChangeState(EnemySpawnerState.Wait);
+            }
+        }
+    }
+
+    private void SpawnEnemy(int2 pos, int code)
+    {
+        Vector3 worldPos = StageManager.instance.GridToWorldPosition(pos);
+        Instantiate(enemies[code]);
+    }
+
+    private void WaveEnd()
+    {
+
+    }
+
+    private void ClearStart()
+    {
+        Debug.Log("스폰 끝");
+    }
+
+    private void ClearUpdate()
+    {
+
+    }
+
+    private void ClearEnd()
+    {
+
+    }
 
     [ContextMenu("Load Wave Data")]
     public void LoadWaveData()
