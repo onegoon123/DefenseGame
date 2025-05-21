8d0e63926888bee6d3c1e97488cd9bb4048791c0
9545000 <asd315406@gmail.com>
2025-05-21 10:14:47 +0900

    UI 업데이트

--- Assets/Code/Scripts/Lobby/CharacterList.cs
+++ Assets/Code/Scripts/Lobby/CharacterList.cs
@@ -10,7 +10,7 @@ public class CharacterList : MonoBehaviour
         for (int i = 0 ; i < transform.childCount ; i++)
         {
             transform.GetChild(i).gameObject.SetActive(DataManager.instance.ContainsPiece(i));
-            Image image = transform.GetChild(i).GetChild(0).GetComponent<Image>();
+            Image image = transform.GetChild(i).GetChild(0).GetChild(0).GetComponent<Image>();
             image.sprite = DataManager.instance.GetPieceData(i).sprite;
         }
     }
